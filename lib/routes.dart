e3b313d9ef34d53939d954569a07de47851f1a3c
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-05-01 09:29:27 +0530

    refactor : update grantPoints route and clean imports

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,10 @@
 // routes.dart
 import 'package:flutter/material.dart';
-import 'package:petropoints/awards.dart';
-import 'package:petropoints/customer.dart';
-import 'package:petropoints/redeem.dart';
 import 'package:petropoints/homepage.dart';
+
+
 class AppRoutes {
-  static const String grantPoints = '/grant-points';
+  static const String grantPoints = '/award';
   static const String dashboard = '/dashboard';
   static const String customers = '/customers';
   static const String redeem = '/redeem';
